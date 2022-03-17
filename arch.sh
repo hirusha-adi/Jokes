29a11896ba6ac3edcc0984a2071ac5b81d26be14
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-03-17 15:02:49 +0530

    installer for arch

--- arch.sh
+++ arch.sh
@@ -1,1 +1,8 @@
-sudo
+echo "Installing Dependencies"
+sudo pacman -Syy install git python python-pip --noconfirm
+echo "Installed git, python, python-pip with apt"
+git clone "https://github.com/hirusha-adi/Jokes.git"
+cd ./Jokes
+pip install -r requirements.txt
+echo "Installed dependencies with pip"
+python app.py
