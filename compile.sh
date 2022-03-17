609470b83d4f96cca00588dea0be55f3909ed2d8
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-03-17 22:58:50 +0530

    update

--- compile.sh
+++ compile.sh
@@ -3,6 +3,11 @@ mkdir compile
 cp ./* ./compile -r
 cd ./compile
 python3 -m PyInstaller --noconfirm --onedir --name "Jokes" ./app.py
-cp ./database ./dist/Jokes/
+cp ./database ./dist/Jokes/ -r
 cp ./config.json ./dist/Jokes/
+rm compile.sh config.json database/ LICENSE __pycache__/ README.md requirements.txt templates/ build/ app.py Jokes.spec README.md compile -rf
+folder_name="$(lsb_release -ds)"
+cd ..
+mkdir "$folder_name"
+mv "./compile/dist/Jokes" "./$folder_name"
 echo "Compiled"
