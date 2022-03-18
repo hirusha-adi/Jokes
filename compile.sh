3cdddf6bc3dfb68ba805e5f84f17b55f43e7ad6d
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-03-18 21:49:07 +0530

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -9,6 +9,8 @@ cp ./config.json ./dist/Jokes/
 rm compile.sh config.json database/ LICENSE __pycache__/ README.md requirements.txt templates/ build/ app.py Jokes.spec README.md compile -rf
 folder_name="$(lsb_release -ds)"
 cd ..
-mkdir "./dist/$folder_name" -r
-mv "./compile/dist/Jokes" "./$folder_name"
+mkdir dist
+cd ./dist
+mkdir "$folder_name"
+mv "../../compile/dist/Jokes" "./$folder_name"
 echo "Compiled"
